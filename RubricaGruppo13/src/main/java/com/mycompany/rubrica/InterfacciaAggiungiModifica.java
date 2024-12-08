cad15b0b2090f625d2ac4664592caeb3b5acdf14
FraPis03 <f.pisacane9@studenti.unisa.it>
2024-12-08 15:39:26 +0100

    Add files via upload

---
+++ RubricaGruppo13/src/main/java/com/mycompany/rubrica/InterfacciaAggiungiModifica.java
@@ -0,0 +1,61 @@
+package com.mycompany.rubrica;
+
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+/**
+ * @class InterfacciaAggiungiModifica
+ * @brief Questa classe rappresenta l'interfaccia per aggiungere o modificare un contatto.
+ *
+ * Tramite questa interfaccia, l'utente potrà modificare i contatti in rubrica.
+ * L'interfaccia è stata implementata come applicazione in JavaFX
+ * 
+ * 
+ * @author Antonio
+ * @date 7 Dicembre 2024
+ * @version 1.0
+ */
+public class InterfacciaAggiungiModifica {
+    ///Attributi
+    Label aggiungiOppureModifica; ///< Etichetta per indicare l'operazione corrente (aggiunta o modifica).
+    VBox[] campi = new VBox[8];   ///< Contenitore VBox per i campi di input. Contiene 8 elementi.
+    Button conferma;              ///< Pulsante per confermare l'operazione.
+    Button annulla;               ///< Pulsante per annullare l'operazione.
+
+    ///Metodi
+    /**
+     * @brief Restituisce il nome inserito dall'utente.
+     * @return Una stringa contenente il nome.
+     */
+    public String getNome() {
+       ///Corpo vuoto
+    }
+
+    /**
+     * @brief Restituisce il cognome inserito dall'utente.
+     * @return Una stringa contenente il cognome.
+     */
+    public String getCognome() {
+       ///Corpo vuoto
+    }
+
+    /**
+     * @brief Restituisce i numeri di telefono inseriti dall'utente.
+     * @return Un array di interi contenente i numeri di telefono.
+     */
+    public int[] getNumeroTelefono() {
+       ///Corpo vuoto
+    }
+
+    /**
+     * @brief Restituisce gli indirizzi email inseriti dall'utente.
+     * @return Un array di stringhe contenente gli indirizzi email.
+     */
+    public String[] getEmail() {
+        ///Corpo vuoto
+    }
+}
+
---
+++ RubricaGruppo13/src/main/java/com/mycompany/rubrica/InterfacciaRubrica.java
@@ -0,0 +1,110 @@
+package com.mycompany.rubrica;
+
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ * @class InterfacciaRubrica
+ * @brief Questa classe rappresenta l'interfaccia princiale della rubrica
+ *
+ * Tramite questa interfaccia, l'utente potrà gestire l'intera rubrica,
+ * grazie agli appositi tasti per aggiungere, modificare ed eliminare contatti,
+ * la casella di ricerca dei contatti, i tasti per scaricare e caricare i contatti.
+ * L'interfaccia è stata implementata come applicazione in JavaFX
+ * 
+ * 
+ * @author Francesco
+ * @date 7 Dicembre 2024
+ * @version 1.0
+ */
+public class InterfacciaRubrica extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        ///Attributi
+        Button aggiungiContatto=new Button("+ Aggiungi Contatto");///< Tasto per aggiungere un nuovo contatto alla rubrica
+        TextField ricerca=new TextField();///< Casella di testo dove inserire il nome o il cognome del contatto da ricercare
+        Button ricercaButton=new Button("Cerca");///< Tasto con cui avviare la ricerca del contatto, in base alla stringa inserita nella casella di ricerca
+        TableView listaContatti=new TableView();///< Una tabella che mostrerà tutti i contatti presenti nella rubrica, in ordine alfabetico
+        Button modificaContatto=new Button("Modifica");///< Tasto per modificare un contatto
+        Button eliminaContatto=new Button("Elimina");///< Tasto per eliminare un contatto
+        Button upload=new Button("Upload file");///< Tasto per caricare un file dal quale estrarre i contatti da aggiungere alla rubrica
+        Button download=new Button("Download contatti");///< Tasto con cui scaricare su un file tutti i contatti della rubrica
+        
+        /** 
+         * Interfaccia che si aprirà quando l'utente clicca sul tasto "Aggiungi contatto" oppure "Modifica",
+         * nel primo caso i campi delle info del contatto risulteranno vuoti,
+         * nel secondo caso saranno precompilati in base alle vecchie info.
+         */
+        InterfacciaAggiungiModifica addOrModify;
+        
+        /**
+         * Interaccia che si aprirà quando l'utente clicca sul tasto "Elimina",
+         * l'interfaccia chiederà conferma prima di eliminare il contatto dalla rubrica.
+        */
+        InterfacciaElimina delete;
+        
+        /**
+         * Interaccia che si aprirà quando l'utente clicca sul tasto "Upload file",
+         * l'interfaccia chiederà all'utente di inserire un file (o un percorso di un file) contenente una lista di contatti.
+         */
+        InterfacciaUpload uploadInt;
+        
+        VBox root=new VBox();
+        root.getChildren().addAll(aggiungiContatto,ricerca,ricercaButton,listaContatti,modificaContatto,eliminaContatto,upload,download);
+        
+       
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        
+        
+        primaryStage.setTitle("Rubrica");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    
+    public static void main(String[] args) {
+        launch(args);
+        
+         /**
+          * @brief Metodo per ottenere il nome del contatto
+          * @return Nome del contatto
+          * */
+        public String getNome(){
+            return "nome contatto";
+        }
+        
+        /**
+          * @brief Metodo per ottenere il cognome del contatto
+          * @return Cognome del contatto
+          * */
+        public String getCogome(){
+            return "cognome contatto";
+        }
+        
+        /**
+          * @brief Metodo per ottenere i numeri di telefono del contatto
+          * @return Numeri di telefono del contatto
+          * */
+        public int[] getNumeroTelefono(){
+            int numeroTelefonoContatto=0123456789;
+            return numeroTelefonoContatto;
+        }
+        
+        /**
+          * @brief Metodo per ottenere le email del contatto
+          * @return Email del contatto
+          * */
+        public String[] getEmail(){
+            return "email contatto";
+        }
+    }
+    
+}
